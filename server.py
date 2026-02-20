5dbfa68ca8bfc03e58d9f48c91aca802bae95df0
Sourabh-Suresh <sourabhthilannur@gmail.com>
2026-02-20 13:13:02 -0500

    Task 7: error handling

--- server.py
+++ server.py
@@ -13,6 +13,8 @@ def emo_detector():
     joy_score = response['joy']
     sadness_score = response['sadness']
     dominant_emotion = response['dominant_emotion']
+    if dominant_emotion is None:
+        return "Invalid text! Please try again!"
     return f"For the given statement, the system response is 'anger': {anger_score}, 'disgust': {disgust_score}, 'fear': {fear_score}, 'joy': {joy_score} and 'sadness': {sadness_score}. The dominant emotion is {dominant_emotion}."
 
 @app.route("/")
@@ -20,4 +22,4 @@ def render_index_page():
     return render_template("index.html")
 
 if __name__ == "__main__":
-    app.run(host="0.0.0.0", port=5000)
+    app.run(host="0.0.0.0", port=5001)
